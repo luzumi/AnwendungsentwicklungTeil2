0de0bb806064d6467b6bead2056255d36bf99bb5
Daniel <corataca@gmail.com>
2020-12-17 17:57:52 +0100

    Drums with mouseClick and select Soudset

--- DrumPad/MainWindow.xaml.cs
+++ DrumPad/MainWindow.xaml.cs
@@ -22,11 +22,15 @@ namespace DrumPad
     public partial class MainWindow : Window
     {
         private MediaPlayer[] mediaPlayers = new MediaPlayer[9];
+        private MediaPlayer drumlessTrack;
         List<string[]> soundSets;
+        private int setSetter = 0;
 
         public MainWindow()
         {
-            InitializeComponent();
+
+
+
             for (int i = 0; i < mediaPlayers.Length; i++)
             {
                 mediaPlayers[i] = new MediaPlayer();
@@ -34,6 +38,8 @@ namespace DrumPad
 
             soundSets = new List<string[]>();
             string[] firstSet = new string[9];
+            string[] secondSet = new string[9];
+            string[] thirdSet = new string[9];
 
             //Drumsamples from https://hiphopmakers.com/free-808-drum-kit-227-samples
             firstSet[0] = @"808Samples/cymbal.wav";
@@ -48,99 +54,101 @@ namespace DrumPad
 
             soundSets.Add(firstSet);
 
-            loadSoundSet(0);
+            secondSet[0] = @"808Samples/cymbal(3).wav";
+            secondSet[1] = @"808Samples/cymbal(4).wav";
+            secondSet[2] = @"808Samples/hihat(12).wav";
+            secondSet[3] = @"808Samples/tom(8).wav";
+            secondSet[4] = @"808Samples/snare(15).wav";
+            secondSet[5] = @"808Samples/snare(32).wav";
+            secondSet[6] = @"808Samples/snare(27).wav";
+            secondSet[7] = @"808Samples/bass(12).wav";
+            secondSet[8] = @"808Samples/tom(12).wav";
+
+            soundSets.Add(secondSet);
+
+            thirdSet[0] = @"808Samples/cymbal(5).wav";
+            thirdSet[1] = @"808Samples/clap(6).wav";
+            thirdSet[2] = @"808Samples/hihat(32).wav";
+            thirdSet[3] = @"808Samples/tom(7).wav";
+            thirdSet[4] = @"808Samples/snare(35).wav";
+            thirdSet[5] = @"808Samples/snare(42).wav";
+            thirdSet[6] = @"808Samples/snare(48).wav";
+            thirdSet[7] = @"808Samples/bass(7).wav";
+            thirdSet[8] = @"808Samples/clap(12).wav";
+
+            soundSets.Add(thirdSet);
+
+            InitializeComponent();
+            
+
+            drumlessTrack = new MediaPlayer();
+            drumlessTrack.Open(new Uri(@"808Samples/Prison_SongDrumless.mp3", UriKind.Relative));
+            drumlessTrack.Play();
+
         }
 
         void PlaySound(object sender, EventArgs e)
         {
-            switch ((sender as Button).Name)
-            {
-                case "bt_0":
-                    playSound(0);
-                    changeButtonColor(0);
-                    break;
-                case "bt_1":
-                    playSound(1);
-                    changeButtonColor(1);
-                    break;
-                case "bt_2":
-                    playSound(2);
-                    changeButtonColor(2);
-                    break;
-                case "bt_3":
-                    playSound(3);
-                    changeButtonColor(3);
-                    break;
-                case "bt_4":
-                    playSound(4);
-                    changeButtonColor(4);
-                    break;
-                case "bt_5":
-                    playSound(5);
-                    changeButtonColor(5);
-                    break;
-                case "bt_6":
-                    playSound(6);
-                    changeButtonColor(6);
-                    break;
-                case "bt_7":
-                    playSound(7);
-                    changeButtonColor(7);
-                    break;
-                case "bt_8":
-                    playSound(8);
-                    changeButtonColor(8);
-                    break;
-            }
+            Int32.TryParse((sender as Button).Name.Substring(3), out int number);
+
+            if(number == 8) loadSoundSet(setSetter);
+
+            mediaPlayers[number].Open(new Uri(soundSets[setSetter][number], UriKind.Relative));
+
+            playSound(number);
+
+            changeButtonColor(number);
+
         }
 
         void changeButtonColor(int count)
         {
+
             switch (count)
             {
                 case 0:
-                    E0.Fill = Brushes.Red;
-                    mediaPlayers[count].MediaEnded += (o, e) => E0.Fill = Brushes.Green;
+                    E0.Fill = Brushes.OrangeRed;
+                    mediaPlayers[count].MediaEnded += (o, e) => E0.Fill = Brushes.DarkGoldenrod;
                     break;
 
                 case 1:
-                    E1.Fill = Brushes.Red;
-                    mediaPlayers[count].MediaEnded += (o, e) => E1.Fill = Brushes.Green;
+                    E1.Fill = Brushes.OrangeRed;
+                    mediaPlayers[count].MediaEnded += (o, e) => E1.Fill = Brushes.DarkGoldenrod;
                     break;
 
                 case 2:
-                    E2.Fill = Brushes.Red;
-                    mediaPlayers[count].MediaEnded += (o, e) => E2.Fill = Brushes.Green;
+                    E2.Fill = Brushes.OrangeRed;
+                    mediaPlayers[count].MediaEnded += (o, e) => E2.Fill = Brushes.DarkGoldenrod;
                     break;
 
                 case 3:
-                    E3.Fill = Brushes.Red;
-                    mediaPlayers[count].MediaEnded += (o, e) => E3.Fill = Brushes.Green;
+                    E3.Fill = Brushes.OrangeRed;
+                    mediaPlayers[count].MediaEnded += (o, e) => E3.Fill = Brushes.DarkGoldenrod;
                     break;
 
                 case 4:
-                    E4.Fill = Brushes.Red;
-                    mediaPlayers[count].MediaEnded += (o, e) => E4.Fill = Brushes.Green;
+                    E4.Fill = Brushes.OrangeRed;
+                    mediaPlayers[count].MediaEnded += (o, e) => E4.Fill = Brushes.DarkGoldenrod;
                     break;
 
                 case 5:
-                    E5.Fill = Brushes.Red;
-                    mediaPlayers[count].MediaEnded += (o, e) => E5.Fill = Brushes.Green;
+                    E5.Fill = Brushes.OrangeRed;
+                    mediaPlayers[count].MediaEnded += (o, e) => E5.Fill = Brushes.DarkGoldenrod;
                     break;
 
                 case 6:
-                    E6.Fill = Brushes.Red;
-                    mediaPlayers[count].MediaEnded += (o, e) => E6.Fill = Brushes.Green;
+                    E6.Fill = Brushes.OrangeRed;
+                    mediaPlayers[count].MediaEnded += (o, e) => E6.Fill = Brushes.DarkGoldenrod;
                     break;
 
                 case 7:
-                    E7.Fill = Brushes.Red;
-                    mediaPlayers[count].MediaEnded += (o, e) => E7.Fill = Brushes.Green;
+                    E7.Fill = Brushes.OrangeRed;
+                    mediaPlayers[count].MediaEnded += (o, e) => E7.Fill = Brushes.DarkGoldenrod;
                     break;
 
                 case 8:
-                    E8.Fill = Brushes.Red;
-                    mediaPlayers[count].MediaEnded += (o, e) => E8.Fill = Brushes.Green;
+                    E8.Fill = Brushes.OrangeRed;
+                    mediaPlayers[count].MediaEnded += (o, e) => E8.Fill = Brushes.DarkGoldenrod;
                     break;
             }
         }
@@ -152,12 +160,20 @@ namespace DrumPad
             mediaPlayers[count].Play();
         }
 
-        void loadSoundSet(int pSoundSet)
+        private void loadSoundSet(int pSoundSet)
         {
-            for (int i = 0; i < mediaPlayers.Length; i++)
+            setSetter++;
+
+            if (setSetter == soundSets.Count)
             {
-                mediaPlayers[i].Open(new Uri(soundSets[pSoundSet][i], UriKind.Relative));
+                setSetter = 0;
+                pSoundSet = 0;
             }
+        }
+
+        private void ToggleButton_OnChecked(object pSender, RoutedEventArgs pE)
+        {
+            loadSoundSet(setSetter);
         }
     }
 }
