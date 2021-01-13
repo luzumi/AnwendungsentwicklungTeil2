39ed794ea2a3c2d61a4b2870ac3120ae3e01df06
Daniel <corataca@gmail.com>
2021-01-13 15:55:37 +0100

    Bindingstart und Übungen

---
+++ Binding/BindingExternal.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Binding
+{
+    /// <summary>
+    /// Interaktionslogik für BindingExternal.xaml
+    /// </summary>
+    public partial class BindingExternal : Page
+    {
+        public BindingExternal(Window bindingTarget = null)
+        {
+            InitializeComponent();
+            DataContext = bindingTarget;
+            WindowContext = bindingTarget;
+        }
+
+
+        public Window WindowContext 
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Binding/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loadBindingIntro(object pSender, RoutedEventArgs pE)
+        {
+            FrameContent.Navigate(new IntroBinding());
+        }
+
+        private void loadBindingIntroExcersice(object pSender, RoutedEventArgs pE)
+        {
+            FrameContent.Navigate(new BindingIntroExercise());
+        }
+
+        private void loadBindingDirection(object pSender, RoutedEventArgs pE)
+        {
+            FrameContent.Navigate(new BindingDirection());
+        }
+
+        private void loadBindingExternal(object pSender, RoutedEventArgs pE)
+        {
+            Page pageToShow = new BindingExternal();
+            FrameContent.Navigate(new BindingExternal(this));
+        }
+    }
+}
