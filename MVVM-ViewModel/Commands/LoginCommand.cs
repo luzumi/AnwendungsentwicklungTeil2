31e3de5fad0074f966bc14308408bb757fcb0a99
Daniel <corataca@gmail.com>
2021-01-20 14:50:33 +0100

    LoginDemo added

---
+++ MVVM-ViewModel/Commands/LoginCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVM_ViewModel
+{
+    public class LoginCommand : ICommand
+    {
+        #region Implementation of ICommand
+        public event EventHandler CanExecuteChanged;
+        public MainViewModel Parent;
+
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Parent.EntryList[Parent.EntryList.Count-1].Name = "eingeloggt";
+        }
+
+
+        #endregion
+    }
+}
---
+++ MVVM-ViewModel/Commands/ModifyUserCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVM_ViewModel
+{
+    public class ModifyUserCommand : ICommand
+    {
+        #region Implementation of ICommand
+
+        public MainViewModel Parent;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Parent.EntryList[(int)parameter].Name = "geändert";
+        } 
+
+
+        #endregion
+    }
+}
